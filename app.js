7b05c8cbd7c6a7e60945b0e16c5fadd92b5c73b0
Jonas Oliveira <jonascsoliveira@outlook.com>
2020-03-09 15:01:59 -0300

    Implementando o metodo de filtro

--- app.js
+++ app.js
@@ -60,6 +60,9 @@ class Bd{
 
         return despesas
     }
+    pesquisar(despesa){
+
+    }
 }
 
 let bd = new Bd()
@@ -87,6 +90,13 @@ function cadastrarDespesa(){
         document.getElementById('modal_btn').innerHTML = 'Voltar'
 
         $('#modalRegistraDespesa').modal('show') 
+        //apagando valores dos campos após fazer inserção.
+        ano.value = ''
+        mes.value = ''
+        dia.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
 
     }else{
 
@@ -130,4 +140,17 @@ function carregaListaDespesas() {
         linha.insertCell(3).innerHTML = d.valor
 
     })
+}
+
+function pesquisarDespesa() {
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+    bd.pesquisar(despesa)
 }
