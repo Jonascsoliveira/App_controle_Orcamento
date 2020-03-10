f85210c5d19b28f6dcaa9ef5e9e373d8a33de82b
Jonas Oliveira <jonascsoliveira@outlook.com>
2020-03-10 15:09:47 -0300

    Adicionando filtro a pesquisa

--- app.js
+++ app.js
@@ -61,7 +61,30 @@ class Bd{
         return despesas
     }
     pesquisar(despesa){
+        let despesasFiltradas = Array()
 
+        despesasFiltradas = this.recuperarTodosRegistros()
+
+        if (despesa.ano != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+        }
+        if (despesa.mes != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
+        }
+        if (despesa.dia != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
+        }
+        if (despesa.tipo != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo)
+        }
+        if (despesa.descricao != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+        }
+        if (despesa.valor != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
+        }
+
+        return despesasFiltradas
     }
 }
 
@@ -110,13 +133,12 @@ function cadastrarDespesa(){
     } 
 }
 
-function carregaListaDespesas() {
-
-    let despesas = Array()
-
-    despesas = bd.recuperarTodosRegistros()
-
+function carregaListaDespesas(despesas = Array(), filtro = false) {
+    if(despesas.length == 0 && filtro == false){
+        despesas = bd.recuperarTodosRegistros()
+    }
     let listaDespesas = document.getElementById('listaDespesas')
+    listaDespesas.innerHTML = ''
     //percorrer array dinamicamente
     despesas.forEach(function (d) {
         //criando linha (tr) e colunas
@@ -152,5 +174,7 @@ function pesquisarDespesa() {
 
     let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
 
-    bd.pesquisar(despesa)
+    let despesas = bd.pesquisar(despesa)
+
+    carregaListaDespesas(despesas, true)
 }
