489d34902d80087bb3de91e321699e29310546a5
Jonas Oliveira <jonascsoliveira@outlook.com>
2020-03-12 14:54:16 -0300

    Finalizando aplicaçao

--- app.js
+++ app.js
@@ -54,7 +54,8 @@ class Bd{
             if (despesa === null) {
                 continue
             }
-
+            
+            despesa.id = i
             despesas.push(despesa)
         }
 
@@ -85,6 +86,10 @@ class Bd{
         }
 
         return despesasFiltradas
+    }
+
+    remover(id){
+        localStorage.removeItem(id)
     }
 }
 
@@ -160,7 +165,20 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
         linha.insertCell(1).innerHTML = d.tipo
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+        
+        //Botão de exclusão
+        let btn = document.createElement("button")
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function () {
+            let id = this.id.replace('id_despesa_', '')
 
+            bd.remover(id)
+
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
     })
 }
 
